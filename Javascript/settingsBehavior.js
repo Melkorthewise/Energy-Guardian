1f9bcbd1bb6f1ca6fa55e21cd1a267bf528667c4
Jarnovis <jd.vis@hotmail.>
2023-11-18 22:00:43 +0100

    Update?

---
+++ Javascript/settingsBehavior.js
@@ -0,0 +1,4 @@
+var interval = setInterval(function() {
+    var div = document.getElementById("settings");
+    div.style.top = div.offsetTop + 1 + "px"},
+    1000);
--- Javascript/test.js
+++
@@ -1,8 +0,0 @@
-var number = 1;
-
-while(true){
-    number++;
-    document.getElementById("test").innerHTML=number;
-    console.log(number);
-    console.log("-1");
-}
