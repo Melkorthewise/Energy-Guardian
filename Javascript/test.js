03f3edb65c55a3282e1b84debaa89f4fb32a0eec
Jarnovis <jd.vis@hotmail.>
2023-11-22 11:47:25 +0100

    help?

---
+++ Javascript/test.js
@@ -0,0 +1,5 @@
+var number = Math.floor(Math.random(0,1)*1000);
+
+export function num(){
+    return number;
+}
---
+++ pyscript-main/pyscript-main/pyscript.core/test/worker.py
@@ -0,0 +1,8 @@
+import a
+from pyscript import display, sync
+
+display("Hello World", target="test", append=True)
+
+print("sleeping")
+sync.sleep(1)
+print("awake")
