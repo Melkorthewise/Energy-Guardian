1bfc6525a05cc375d3d5075ba20f021c5f39bcc8
Jarnovis <jd.vis@hotmail.>
2023-11-23 15:20:05 +0100

    sinc

--- Javascript/settingsBehavior.js
+++ Javascript/settingsBehavior.js
@@ -1,4 +1,14 @@
-var interval = setInterval(function() {
-    var div = document.getElementById("settings");
-    div.style.top = div.offsetTop + 1 + "px"},
-    1000);
+var y = 0;
+var change = true;
+if(y==500){
+    change = false;
+}
+
+else{
+    change = true;
+}
+
+export default function output(){
+    var tekst = "output"
+    return tekst;
+}
