ae47d901798306c8e829bc29b725ab0ba1e5248f
Jarnovis <jd.vis@hotmail.>
2023-11-25 18:12:45 +0100

    Importing format voor gebruik database

---
+++ testDatabase.js
@@ -0,0 +1,83 @@
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"JHKest24", //eigen wachtwoord
+    database: "test"
+
+});
+
+//Creating database
+//con.connect(function(err){
+    //if(err) throw err;
+    //console.log("Connected!");
+
+    //con.query("CREATE DATABASE test", function (err, result){
+        //if(err) throw err;
+        //console.log("Database");
+    //})
+//});
+
+//Adding tables
+//con.connect(function(err){
+    //if(err) throw err;
+    //var sql= "CREATE TABLE User (UserName VARCHAR(255), PASSWORD VARCHAR(255))";
+
+    //con.query(sql, function(err, result){
+        //if (err) throw err;
+        //console.log("Table created");
+    //});
+//});
+
+//Altering tables
+//con.connect(function(err){
+    //if (err) throw err;
+
+    //var sql = "ALTER TABLE User ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY"; //Geeft automatisch een unieke sleutel
+    //con.query(sql, function(err, result){
+        //if (err) throw err;
+        //console.log("Table altered");
+    //});
+//});
+
+//Inserting in tables
+//con.connect(function(err){
+    //if (err) throw err;
+
+    //var sql = "INSERT INTO User (Username, PASSWORD) VALUES ('Frederik', 'Netflix&Chill')";
+    //con.query(sql, function(err, result){
+        //if (err) throw err;
+        //console.log("1 record inserted" + result);
+    //});
+//});
+
+//Selecting from table
+//con.connect(function(err){
+    //if (err) throw err;
+
+    //con.query("SELECT * FROM User", function(err, result){
+        //if (err) throw err;
+        //console.log(result);
+    //});
+//});
+
+//Select with filter
+//con.connect(function(err){
+    //if (err) throw err;
+
+    //con.query("SELECT * FROM User WHERE UserName='Hendrik'", function(err, result){
+        //if (err) throw err;
+        //console.log(result);
+    //});
+//});
+
+//Sort result
+con.connect(function(err){
+    if (err) throw err;
+
+    con.query("SELECT * FROM User ORDER BY UserName", function(err, result){
+        if (err) throw err;
+        console.log(result);
+    });
+});
